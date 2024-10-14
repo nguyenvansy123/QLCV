1f5000368db0993dfec823091d932c4780272c0d
khth7 <55275100+nguyenvansy123@users.noreply.github.com>
2024-10-14 16:12:36 +0700

    update code

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid2';
+import {Sidebar} from './Sidebar'
+import {Main} from './Main'
+import './style.css'
 
 export const Layout = () => {
     return (
         <Grid container spacing={2}>
             <Grid size={3}>
-                
+               <Sidebar></Sidebar>
             </Grid>
             <Grid size={9}>
-
+                <Main></Main>
             </Grid>
         </Grid>
     )
--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Sidebar = () => {
+export const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div id='sidebar'>
+            <ul>
+                <li className='active'><a href="#">Thống kê</a></li>
+                <li><a href="#">Quy trình mẫu</a></li>
+                <li><a href="#">Hành chính</a></li>
+                <li><a href="#">Chuyên môn</a></li>
+                <li><a href="#">NCKH</a></li>
+                <li><a href="#">Đào tạo</a></li>
+                <li><a href="#">Hợp tác quốc tế</a></li>
+                <li><a href="#">Báo cáo, thống kê, lưu trữ, thư viện</a></li>
+                <li><a href="#">Công vệc khác</a></li>
+            </ul>
+
+        </div>
     )
 }
-
-export default Sidebar
